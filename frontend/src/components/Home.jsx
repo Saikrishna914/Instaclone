dd12169b9aef9839acc30732477914e9e2c3d931
Sai Krishna <saikrishnajaihind2424@gmail.com>
2025-07-23 23:16:48 +0530

    Added some components

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Feed from './Feed'
+import RightSideBar from './RightSideBar'
+import useGetAllPost from '@/hook/useGetAllPost'
+
+const Home = () => {
+    useGetAllPost();
+    return (
+        <div className='flex'>
+            <div className='flex-grow'>
+                <Feed />
+                <Outlet />
+            </div>
+            <RightSideBar />
+        </div>
+    )
+}
+
+export default Home
