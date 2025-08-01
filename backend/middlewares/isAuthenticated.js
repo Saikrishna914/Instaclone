72cddc4afd44ac1caaf682500a7833f4f61c8ef3
Sai Krishna <165396928+Saikrishna914@users.noreply.github.com>
2025-08-02 01:27:19 +0530

    Fixed Bugs : Updated isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 const isAuthenticated =  async (req,res,next) => {
     try{
-        console.log (req.cookies.token);
         const token = req.cookies.token;
         if(!token){
             return res.status(401).json({
